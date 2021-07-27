f25ab8bebf6956acd107a23c72ac4b7819724398
Nicolasmaes <n.maes@codeur.online>
2021-07-27 12:32:56 +0200

    ajout du déplacement des ennemis

--- bomberman/moveEnemies.js
+++ bomberman/moveEnemies.js
@@ -1,43 +1,42 @@
-setInterval(moveEnemies, 1000); 
+setInterval(moveEnemies,1000);
 
 function moveEnemies() {
-    const enemies=document.getElementsByClassName('enemies');
     for (let i = 0; i < enemies.length; i++) {
-        let random=Math.floor(Math.random()*4);
-        // console.log(random);
-         switch (random) {
-                 case 0:
-                     if (enemies[i].offsetLeft <= 720 ) {
-                 enemies[i].style.top=enemies[i].offsetTop +40 +'px';
-                 enemies[i].style.left=enemies[i].offsetLeft;  
-             }
-                 
-                 break;
-                 case 1:
-                     if ( enemies[i].offsetTop <= 720 ) {
-                 enemies[i].style.top=enemies[i].offsetTop +40 +'px';
-                 enemies[i].style.left=enemies[i].offsetLeft;  
-             }
-                 
-                 break;
-                 case 2:
-                     if ( enemies[i].offsetLeft >= 24  ) {
-                 enemies[i].style.top=enemies[i].offsetTop;
-                 enemies[i].style.left=enemies[i].offsetLeft -40 +'px';  
-             }
-                 
-                 break;
-             case 3:
-                 if (enemies[i].offsetTop >= 24 ) {
-                 enemies[i].style.top=enemies[i].offsetTop;
-                 enemies[i].style.left=enemies[i].offsetLeft -40 +'px';  
-                 }
-                 
-                 break;
-            
-         
-             default:
-                 break;
-        }   
+        const enemies=document.getElementsByClassName('enemies');
+       let random=Math.floor(Math.random()*4);
+       // console.log(random);
+        switch (random) {
+                case 0:
+                    if (enemies[i].offsetLeft <= 550 ) {
+                enemies[i].style.top=enemies[i].offsetTop +40 +'px';
+                enemies[i].style.left=enemies[i].offsetLeft +'px';  
+            }
+                
+                break;
+                case 1:
+                    if ( enemies[i].offsetTop <= 550 ) {
+                enemies[i].style.top=enemies[i].offsetTop +'px';
+                enemies[i].style.left=enemies[i].offsetLeft +40 +'px';  
+            }
+                
+                break;
+                case 2:
+                    if ( enemies[i].offsetLeft >= 10  ) {
+                enemies[i].style.top=enemies[i].offsetTop +'px';
+                enemies[i].style.left=enemies[i].offsetLeft -40 +'px';  
+            }
+                
+                break;
+            case 3:
+                if (enemies[i].offsetTop >= 10 ) {
+                enemies[i].style.top=enemies[i].offsetTop -40 +'px';
+                enemies[i].style.left=enemies[i].offsetLeft +'px';  
+                }
+                
+                break;
+           
+        
+            default:
+        }
     }
 }
